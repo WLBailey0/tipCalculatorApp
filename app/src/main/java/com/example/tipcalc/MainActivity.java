8cc2cb0f96a591a1c18fd7d2e320c426e949f813
WLBailey0 <WLBailey0@gmail.com>
2021-07-19 20:54:24 -0500

    Removed app title from app screen

--- app/src/main/java/com/example/tipcalc/MainActivity.java
+++ app/src/main/java/com/example/tipcalc/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -15,6 +16,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
 
         Button good = (Button) findViewById(R.id.good);
